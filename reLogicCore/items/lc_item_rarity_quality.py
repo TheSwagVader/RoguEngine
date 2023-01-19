e900b1f5688ac9a6fee7a0eeb08d04e92151b55d
TheSwagVader <egorzverev666@gmail.com>
2023-01-19 12:45:25 +0300

    Improved itemQuality class

--- reLogicCore/items/lc_item_rarity_quality.py
+++ reLogicCore/items/lc_item_rarity_quality.py
@@ -17,9 +17,10 @@ class itemRarity(Enum):
     Champion,
     Unique = range(15)
     #Trash, Common, Uncommon, Rare, Very rare, Super rare, Epic, Legendary, Mythic, Divine, Eldritch
-    # Fractal, Darkest, Champion, Unique
+    # Fractal, Darkest, Champion, Unique; May be more
 
-quialityLevels = [
+
+ITEM_QUALITY_LEVELS = [
     'bronze',
     'silver',
     'gold',
@@ -29,5 +30,20 @@ quialityLevels = [
 class itemQuality:
     def __init__(self, level):
         self.__stars = 5 if level % 5 == 0 else level % 5
-        self.__qLevel = quialityLevels[level // 5]
+        self.__qLevel = level // 5
+        self.__qLevelName = ITEM_QUALITY_LEVELS[level // 5]
+
+    def getStars(self):
+        return self.__stars
+    
+    def gerQualityLevel(self):
+        return self.__qLevelName
+    
+    def raiseQuality(self):
+        self.__stars += 1
+        if self.__stars > 5:
+            self.__stars = 1
+            if self.__qLevel < 4:
+                self.__qLevel += 1
+                self.__qLevelName = ITEM_QUALITY_LEVELS[self.__qLevel]
     # 1 level = 5 stars; levels: bronze, silver, gold, platinum, diamond
