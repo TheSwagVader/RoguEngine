c10400b1d0847495b040a27b9bda11fb3663b92a
TheSwagVader <egorzverev666@gmail.com>
2023-01-20 17:06:44 +0300

    Added all armor types to ITEM_TYPES list in lc_it_sysconst.py

--- reLogicCore/items/lc_it_sysconst.py
+++ reLogicCore/items/lc_it_sysconst.py
@@ -1,3 +1,28 @@
 ITEM_TYPES = [
-    
+    'armory_helmet',
+    'armory_chestplate',
+    'armory_gloves',
+    'armory_leggins',
+    'armory_boots',
+    'armor_battle_belt',
+    'armor_ring_left_hand_thumb_finger',
+    'armor_ring_left_hand_index_finger',
+    'armor_ring_left_hand_middle_finger',
+    'armor_ring_left_hand_ring_finger',
+    'armor_ring_left_hand_little_finger',
+    'armor_ring_right_hand_thumb_finger',
+    'armor_ring_right_hand_index_finger',
+    'armor_ring_right_hand_middle_finger',
+    'armor_ring_right_hand_ring_finger',
+    'armor_ring_right_hand_little_finger',
+    'armor_talisman',
+    'armor_force_field'
+]
+
+FINGERS = [
+    'thumb',
+    'index',
+    'middle',
+    'ring',
+    'little'
 ]
