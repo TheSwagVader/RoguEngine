6084c1495d96df30e5c03cc3340c3e88b4483b0e
TheSwagVader <egorzverev666@gmail.com>
2023-01-24 15:46:12 +0300

    Added item classes to do later

--- reLogicCore/items/lc_item.py
+++ reLogicCore/items/lc_item.py
@@ -1,1 +1,26 @@
 from lc_base_item import BaseItem
+
+#Todo item types potions and other shit
+class Potion(BaseItem):
+    pass
+
+class Bandage(BaseItem):
+    pass
+
+class Antidote(BaseItem):
+    pass
+
+class Seal(BaseItem):
+    pass
+
+class Book(BaseItem):
+    pass
+
+class Scroll(BaseItem):
+    pass
+
+class Currency(BaseItem):
+    pass
+
+class Artifact(BaseItem):
+    pass
